43a9d4b452489ce1feff4e28e5ca44d5efcd9166
Victor Limonti <victor_limonti@hotmail.com>
2022-09-03 10:20:40 -0300

    Lista de projetos dinâmica

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div
-        class="column is-8"
-        role="form"
-        aria-label="Formulário para criação de uma nova tarefa"
-      >
-        <input
-          type="text"
-          class="input"
-          placeholder="Qual tarefa você deseja iniciar?"
-          v-model="descricao"
-        />
+      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
+          <input
+            type="text"
+            class="input"
+            placeholder="Qual tarefa você deseja iniciar?"
+            v-model="descricao"
+          />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="column">
         <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
@@ -21,8 +27,10 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Temporizador from './Temporizador.vue'
+import { useStore } from 'vuex'
+import { key } from '@/store'
 
 export default defineComponent({
   name: "Formulário",
@@ -32,16 +40,24 @@ export default defineComponent({
   },
   data () {
     return {
-      descricao: ''
+      descricao: '',
+      idProjeto: ''
     }
   },
   methods: {
     finalizarTarefa (tempoDecorrido: number) : void {
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
+        descricao: this.descricao,
+        projeto: this.projetos.find(proj => proj.id == this.idProjeto)
       })
       this.descricao = ''
+    }
+  },
+  setup () {
+    const store = useStore(key)
+    return {
+      projetos: computed(() => store.state.projetos)
     }
   }
 });
--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -37,25 +37,27 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from "@/store"
 
-import IProjeto from '../interfaces/IProjeto'
 export default defineComponent({
     name: "Projetos",
     data () {
         return { 
-            nomeDoProjeto: "",
-            projetos: [] as IProjeto[]
-        }
+            nomeDoProjeto: ""
+        };
     },
     methods: {
         salvar () {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
+            this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
             this.nomeDoProjeto = ''
+        },
+    },
+    setup () {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos )
         }
     }
 });
